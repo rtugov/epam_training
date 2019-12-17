43bb8fb6fe771261a47bb25d49b5c5ec2076a852
Tugov Roman <TugovRoman@gmail.com>
2019-12-17 16:06:48 +0300

    Add the first programming task from introduction to Java

---
+++ Task3/src/com/rtugov/Main.java
@@ -0,0 +1,11 @@
+package com.rtugov;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //Print app arguments.
+        for (int i=0; i<args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
